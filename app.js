05582635ee4923822ca3cf9468a6f088c59dbd77
thamma <thammaraku@gmail.com>
2021-01-08 23:49:54 -0600

    add search by manager and banner

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Import package
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+require('console.table');
 
 // Connect database
 const connection = mysql.createConnection({
@@ -16,11 +17,6 @@ connection.connect((err) => {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
 
-    // viewEmployees();
-    // viewRoles();
-    // viewDepartments();
-  
-
     start();
 
 });
@@ -29,10 +25,27 @@ connection.connect((err) => {
 
 function start() {
 
+    console.log(",—--------------------------------------------------------.");
+    console.log("|  ###### #    # #####  #       ####  #   # ###### ###### |");
+    console.log("|  #      ##  ## #    # #      #    #  # #  #      #      |");
+    console.log("|  #####  # ## # #    # #      #    #   #   #####  #####  |");
+    console.log("|  #      #    # #####  #      #    #   #   #      #      |");
+    console.log("|  #      #    # #      #      #    #   #   #      #      |");
+    console.log("|  ###### #    # #      ######  ####    #   ###### ###### |");
+    console.log("|                                                         |");
+    console.log("|    #    #   ##   #    #   ##    ####  ###### #####      |");
+    console.log("|    ##  ##  #  #  ##   #  #  #  #    # #      #    #     |");
+    console.log("|    # ## # #    # # #  # #    # #      #####  #    #     |");
+    console.log("|    #    # ###### #  # # ###### #  ### #      #####      |");
+    console.log("|    #    # #    # #   ## #    # #    # #      #   #      |");
+    console.log("|    #    # #    # #    # #    #  ####  ###### #    #     |");
+    console.log("‘—--------------------------------------------------------‘");
+
+
     inquirer
         .prompt({
             name: "userPick",
-            type: "rawlist",
+            type: "list",
             message: "What would you like to do?",
             choices: [
                 "View All Employee",
@@ -52,11 +65,16 @@ function start() {
 
                 case "View All Employee":
                     viewEmployees();
-                    connection.end();
                     break;
 
-                default:
+                case "View All Employee By Department":
+                    viewDepartment();
+                    break;
+
+                case "View All Employee By Manager":
+                    viewEmployees();
                     connection.end();
+                    break;
 
             };
         });
@@ -66,7 +84,103 @@ function start() {
 
 // SQL QUERY PART
 async function viewEmployees() {
-    const SQL_STATEMENT = "SELECT * FROM employee";
+    const SQL_STATEMENT = `SELECT employee.id, first_name, last_name, title, department.department, role.salary, manager.manager 
+    FROM (((employee 
+    INNER JOIN role 
+    ON employee.role_id = role.id) 
+    INNER JOIN department 
+    ON role.department_id = department.id) 
+    LEFT JOIN manager 
+    ON employee.manager_id = manager.id)`;
+
     const [rows, fields] = await connection.promise().query(SQL_STATEMENT);
+    console.log("\t");
     console.table(rows);
+    connection.end();
 }
+
+
+async function viewDepartment() {
+
+    inquirer
+        .prompt({
+            name: "departmentSelect",
+            type: "list",
+            message: "Which department would you like to search?",
+            choices: [
+                "Sales",
+                "Engineering",
+                "Finance",
+                "Legal"
+            ]
+        })
+
+        .then(async function (answer) {
+
+            const SQL_STATEMENT = `SELECT employee.id, first_name, last_name, title, department.department 
+            FROM ((employee 
+            INNER JOIN role 
+            ON employee.role_id = role.id) 
+            INNER JOIN department 
+            ON role.department_id = department.id) 
+            WHERE department.department = "${answer.departmentSelect}"`;
+
+            const [rows, fields] = await connection.promise().query(SQL_STATEMENT);
+            console.log("\t");
+            console.table(rows);
+            connection.end();
+        });
+}
+
+
+
+// function searchDepartment() {
+
+//     inquirer
+//         .prompt({
+//             name: "departmentSelect",
+//             type: "list",
+//             message: "Which department would you like to search?",
+//             choices: [
+//                 "Sales",
+//                 "Engineering",
+//                 "Finance",
+//                 "Add Employee",
+//                 "Legal"
+//             ]
+//         })
+
+//         .then(function(answer) {
+//             console.log(answer);
+//             console.log(answer.departmentSelect);
+//             const departmentSelect = answer.departmentSelect;
+//             viewEmployeesDepartment(departmentSelect)
+//         });
+// }
+
+
+
+
+// ######### BACKUP CODE ###########
+
+//This line of code runs a synchronous function through the figlet npm that displays the designated text string in the console
+// const figlet = require('figlet');
+// console.log(figlet.textSync('Employee Management', {
+//     font: 'Standard',
+//     horizontalLayout: 'default',
+//     verticalLayout: 'default'
+// }));
+
+// #### OR #####
+// console.log(figlet.textSync('Employee Manager', {
+//     font: '3D-ASCII',
+//     horizontalLayout: 'default',
+//     verticalLayout: 'default',
+//     width: 100,
+//     whitespaceBreak: true
+// }))
+
+
+
+
+
